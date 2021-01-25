1723c629a87ba4e402abf5c7c137600a09e5ff42
Cássia <cascrisbern@gmail.com>
2021-01-25 15:13:41 -0300

    it was worked on the structure of the list of ads css for the list of ads placing an order animations using lottie

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,6 +3,7 @@ const { hash } = require('bcryptjs')
 
 const User = require('../models/user')
 const Product = require('../models/Product')
+const LoadProductsService = require('../services/LoadProductService')
 
 const { formatCep, formatCpfCnpj } = require('../lib/utils')
 
@@ -117,5 +118,13 @@ module.exports = {
         error: "Erro ao tentar deletar sua conta!"
       })
     }
+  },
+
+  async ads(req,res) {
+    const products = await LoadProductsService.load('products', {
+      where: { user_id: req.session.userId }
+    })
+
+    return res.render("user/ads", { products })
   }
 }
